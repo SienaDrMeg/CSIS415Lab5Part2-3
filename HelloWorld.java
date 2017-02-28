8bac50a3da6610d47fcfbcafdc02f8dc6da0ad76
aa29cala <aa29cala@siena.edu>
2017-02-28 17:31:56 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public static void main(String[] args)
+    {
+        // initialise instance variables
+        System.out.println("hello world");
+    }
+}
